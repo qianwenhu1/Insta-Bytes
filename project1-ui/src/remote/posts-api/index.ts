9a0a516aa89f0a63a6f9673f624b41e275ae1422
Reznitchenko <inna.reznitchenko@accenture.com>
2020-08-05 16:33:52 -0400

    added some google maps api functionality and yaml file

--- project1-ui/src/remote/posts-api/index.ts
+++ project1-ui/src/remote/posts-api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { lbBaseUrl } from '../../environment'
 
 export const postClient = axios.create({
-    baseURL:'http://localhost:80/post-service',
+    baseURL:'http://localhost:8070/post-service',
     headers:{
         'Content-Type': 'application/json'
     },
