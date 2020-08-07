ed9e117193d34e830945232d078c5d8283bd5a98
Reznitchenko <inna.reznitchenko@accenture.com>
2020-08-07 10:49:37 -0400

    changed port back

--- project1-ui/src/environment.ts
+++ project1-ui/src/environment.ts
@@ -1,7 +1,7 @@
 export let lbBaseUrl:string
 
 if(process.env['NODE_ENV'] === 'production'){
-    lbBaseUrl = 'http://instaBytes.innaconnection.com'
+    lbBaseUrl = 'http://app-service.innaconnection.com'
 }else {
     lbBaseUrl = 'http://localhost:8070/user-service'
 }
