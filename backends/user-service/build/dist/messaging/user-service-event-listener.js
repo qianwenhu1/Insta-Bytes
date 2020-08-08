b351bf39910e90f99762a4d83b60691a0a54e591
Reznitchenko <inna.reznitchenko@accenture.com>
2020-08-07 21:01:22 -0400

    build user

--- backends/user-service/build/dist/messaging/user-service-event-listener.js
+++ backends/user-service/build/dist/messaging/user-service-event-listener.js
@@ -1,14 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var _1 = require(".");
-_1.userSubscription.on('message', function (message) {
+var loggers_1 = require("../utils/loggers");
+// import { Message } from "@google-cloud/pubsub";
+function onMessage(message) {
     try {
         var parsedData = JSON.parse(Buffer.from(message.data, 'base64').toString());
-        console.log(parsedData);
+        loggers_1.logger.debug("Received New Post: " + parsedData);
         message.ack();
     }
     catch (e) {
         message.nack();
     }
-});
+}
+_1.userSubscription.on('message', onMessage);
 //# sourceMappingURL=user-service-event-listener.js.map
